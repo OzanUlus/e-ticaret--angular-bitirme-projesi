75f90c6db1934355d88ee338ef295caaf268dc72
OzanUlus <ilkerozanulus@gmail.com>
2025-08-01 16:23:48 +0300

    category get and routing

--- apps/admin/src/app.routes.ts
+++ apps/admin/src/app.routes.ts
@@ -16,6 +16,10 @@ export const appRoutes: Route[] = [
       {
         path: 'products',
         loadChildren: () => import('./pages/products/route')
+      },
+       {
+        path: 'categories',
+        loadChildren: () => import('./pages/categories/routes')
       }
      
     ],
--- apps/admin/src/navigation.ts
+++ apps/admin/src/navigation.ts
@@ -1,18 +1,23 @@
-export interface NavigationModel{
-    title: string;
-    url: string;
-    icon: string;
+export interface NavigationModel {
+  title: string;
+  url: string;
+  icon: string;
 }
 
 export const navigations: NavigationModel[] = [
-    {
-        title: "Ana Sayfa",
-        url: "/",
-        icon: "home"
-    },
-      {
-        title: "Ürünler",
-        url: "/products",
-        icon: "deployed_code"
-    }
-]
+  {
+    title: 'Ana Sayfa',
+    url: '/',
+    icon: 'home',
+  },
+  {
+    title: 'Ürünler',
+    url: '/products',
+    icon: 'deployed_code',
+  },
+  {
+    title: 'Kategoriler',
+    url: '/categories',
+    icon: 'category',
+  },
+];
