f6020f07501b14a80692d98aedaf87cc1c8c8e91
OzanUlus <ilkerozanulus@gmail.com>
2025-08-08 12:03:49 +0300

    payment adn show bsket on payment page

--- apps/ui/src/pages/baskets/baskets.ts
+++ apps/ui/src/pages/baskets/baskets.ts
@@ -1,17 +1,22 @@
 import { HttpClient, httpResource } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, computed, inject, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Common } from '../../service/common';
 import { BasketModel } from '@shared/models/basket.model';
 import { TrCurrencyPipe } from 'tr-currency';
 import { FlexiToastService } from 'flexi-toast';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [
-    TrCurrencyPipe
-  ],
+  imports: [TrCurrencyPipe, RouterLink],
   templateUrl: './baskets.html',
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Baskets {
   readonly result = httpResource<BasketModel[]>(() => {
@@ -22,38 +27,43 @@ export default class Baskets {
 
   readonly total = computed(() => {
     let val = 0;
-    this.data().forEach(res => {
-      val+= res.productPrice * res.quantity
+    this.data().forEach((res) => {
+      val += res.productPrice * res.quantity;
     });
 
     return val;
   });
-  readonly kdv = computed(() => this.total() * 18 / 100);
+  readonly kdv = computed(() => (this.total() * 18) / 100);
 
   readonly #common = inject(Common);
 
-   readonly #http = inject(HttpClient);
+  readonly #http = inject(HttpClient);
   readonly #toast = inject(FlexiToastService);
 
-  increment(val: BasketModel){
+  increment(val: BasketModel) {
     val.quantity++;
-    this.#http.put(`api/baskets/${val.id}`,val).subscribe(() => {
+    this.#http.put(`api/baskets/${val.id}`, val).subscribe(() => {
       this.result.reload();
     });
   }
 
-  decrement(val: BasketModel){
+  decrement(val: BasketModel) {
     const count = val.quantity - 1;
 
-    if(count <= 0){
-      this.#toast.showSwal("Sil?","Ürünü sepetten silmek istiyor musunuz?","Sil",() => {
-        this.#http.delete(`api/baskets/${val.id}`).subscribe(() => {
-          this.result.reload();
-        });
-      })
-    }else{
+    if (count <= 0) {
+      this.#toast.showSwal(
+        'Sil?',
+        'Ürünü sepetten silmek istiyor musunuz?',
+        'Sil',
+        () => {
+          this.#http.delete(`api/baskets/${val.id}`).subscribe(() => {
+            this.result.reload();
+          });
+        }
+      );
+    } else {
       val.quantity--;
-      this.#http.put(`api/baskets/${val.id}`,val).subscribe(() => {
+      this.#http.put(`api/baskets/${val.id}`, val).subscribe(() => {
         this.result.reload();
       });
     }
